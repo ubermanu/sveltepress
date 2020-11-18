0cdc98e260e1620f63d8281c516283e327f8b053
ubermanu <e.vodor@gmail.com>
2020-11-18 20:02:43 +0100

    Remove pages local json

--- src/routes/pages.svelte
+++ src/routes/pages.svelte
@@ -1,10 +1,18 @@
 <script context="module">
-  export function preload() {
-    return this.fetch(`pages.json`)
-      .then((r) => r.json())
-      .then((pages) => {
-        return { pages };
-      });
+  import WPAPI from "wpapi";
+
+  // TODO: Create a store to access this endpoint
+  var wp = new WPAPI({
+    endpoint: "http://localhost:8080/index.php?rest_route=",
+  });
+
+  export async function preload() {
+    // TODO: Add pagination support
+    const pages = await wp.pages().then((pages) => {
+      return pages.filter((_, id) => id !== "_paging");
+    });
+
+    return { pages };
   }
 </script>
 
